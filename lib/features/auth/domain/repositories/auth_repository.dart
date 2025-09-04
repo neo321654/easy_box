4becb457631503d0b2c2833e529f6bcf882226da
neo321654 <neo321654@rambler.ru>
2025-09-04 12:38:48 +0300

    refactor(auth): improve login error handling

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:easy_box/core/error/failures.dart';
 import 'package:easy_box/features/auth/domain/entities/user.dart';
 
 abstract class AuthRepository {
-  Future<User> login({
+  Future<Either<Failure, User>> login({
     required String email,
     required String password,
   });
