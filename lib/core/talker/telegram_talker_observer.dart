89cb732e5c2eb07330ef22c593b83b3d21e08c11
neo321654 <neo321654@rambler.ru>
2025-09-05 22:18:19 +0300

    fix: Ensure async log sending is not blocked by sync observer

--- lib/core/talker/telegram_talker_observer.dart
+++ lib/core/talker/telegram_talker_observer.dart
@@ -1,9 +1,27 @@
+import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 class TelegramTalkerObserver extends TalkerObserver {
   final Dio _dio = Dio();
   final String _url = 'http://38.244.208.106:8000/log-client-error';
+
+  void _sendToTelegram(String message) {
+    // We wrap the async call in a Future to ensure it's scheduled
+    // on the event loop from a synchronous observer.
+    Future(() async {
+      try {
+        await _dio.post(
+          _url,
+          data: {'message': message},
+        );
+      } catch (e) {
+        // Avoid loops, just print to console if sending fails
+        // ignore: avoid_print
+        print('Failed to send log to Telegram: $e');
+      }
+    });
+  }
 
   @override
   void onLog(TalkerData log) {
@@ -22,18 +40,5 @@ class TelegramTalkerObserver extends TalkerObserver {
   @override
   void onException(TalkerException err) {
     _sendToTelegram(err.generateTextMessage());
-  }
-
-  void _sendToTelegram(String message) {
-    try {
-      _dio.post(
-        _url,
-        data: {'message': message},
-      );
-    } catch (e) {
-      // Avoid loops, just print to console if sending fails
-      // ignore: avoid_print
-      print('Failed to send log to Telegram: $e');
-    }
   }
 }
