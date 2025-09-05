b771fb8b014aba3ddcf5285f9efebf7b0c7ac53b
neo321654 <neo321654@rambler.ru>
2025-09-05 23:41:27 +0300

    fix(inventory): Обновление списка и редактирование фото
    Реализован возврат обновленного объекта товара со страницы сведений, что обеспечивает автоматическое обновление списка инвентаря.
    
    - Добавлена функция выбора нового изображения в режиме редактирования товара.
    - Устранены предупреждения анализатора заменой `WillPopScope` на `PopScope` и использованием `onPopInvokedWithResult`.

--- lib/features/inventory/presentation/pages/product_detail_page.dart
+++ lib/features/inventory/presentation/pages/product_detail_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:easy_box/core/extensions/context_extension.dart';
 import 'package:easy_box/core/utils/app_dimensions.dart';
 import 'package:easy_box/core/widgets/app_snack_bar.dart';
+import 'package:easy_box/core/widgets/image_source_sheet.dart';
 import 'package:easy_box/core/widgets/widgets.dart';
 import 'package:easy_box/di/injection_container.dart';
 import 'package:easy_box/features/inventory/domain/entities/product.dart';
@@ -8,6 +11,7 @@ import 'package:easy_box/features/inventory/presentation/bloc/product_detail_blo
 import 'package:easy_box/features/inventory/presentation/widgets/product_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductDetailPage extends StatelessWidget {
   final Product product;
@@ -35,6 +39,7 @@ class _ProductDetailView extends StatefulWidget {
 class _ProductDetailViewState extends State<_ProductDetailView> {
   late Product _currentProduct;
   bool _isEditing = false;
+  File? _imageFile;
 
   late final TextEditingController _nameController;
   late final TextEditingController _skuController;
@@ -46,7 +51,8 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
     _currentProduct = widget.product;
     _nameController = TextEditingController(text: _currentProduct.name);
     _skuController = TextEditingController(text: _currentProduct.sku);
-    _locationController = TextEditingController(text: _currentProduct.location);
+    _locationController =
+        TextEditingController(text: _currentProduct.location);
   }
 
   @override
@@ -57,14 +63,27 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
     super.dispose();
   }
 
+  Future<void> _pickImage() async {
+    final source = await showImageSourceSheet(context);
+    if (source == null) return;
+
+    final pickedFile = await ImagePicker().pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
   void _toggleEditMode() {
     setState(() {
       _isEditing = !_isEditing;
       if (!_isEditing) {
-        // Reset controllers if canceling edit
+        // Reset controllers and image file if canceling edit
         _nameController.text = _currentProduct.name;
         _skuController.text = _currentProduct.sku;
         _locationController.text = _currentProduct.location ?? '';
+        _imageFile = null;
       }
     });
   }
@@ -76,15 +95,17 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
       sku: _skuController.text,
       quantity: _currentProduct.quantity,
       location: _locationController.text,
-      imageUrl: _currentProduct.imageUrl,
+      imageUrl: _imageFile?.path ?? _currentProduct.imageUrl,
     );
-    context.read<ProductDetailBloc>().add(UpdateProductRequested(updatedProduct));
-    _toggleEditMode();
+    context
+        .read<ProductDetailBloc>()
+        .add(UpdateProductRequested(updatedProduct));
   }
 
   void _updateQuantity(int change) {
     final newQuantity = _currentProduct.quantity + change;
     if (newQuantity >= 0) {
+      // We create a new Product object with the updated quantity
       final updatedProduct = Product(
         id: _currentProduct.id,
         name: _currentProduct.name,
@@ -93,7 +114,10 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
         location: _currentProduct.location,
         imageUrl: _currentProduct.imageUrl,
       );
-      context.read<ProductDetailBloc>().add(UpdateProductRequested(updatedProduct));
+      // We dispatch an event to the BLoC to handle the update
+      context
+          .read<ProductDetailBloc>()
+          .add(UpdateProductRequested(updatedProduct));
     }
   }
 
@@ -115,78 +139,103 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_isEditing ? context.S.editProductDialogTitle : _currentProduct.name),
-        actions: [
-          if (_isEditing)
-            IconButton(
-              icon: const Icon(Icons.cancel),
-              onPressed: _toggleEditMode,
-            )
-          else
-            IconButton(
-              icon: const Icon(Icons.edit),
-              onPressed: _toggleEditMode,
-            ),
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: _showDeleteConfirmationDialog,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) {
+          return;
+        }
+        Navigator.of(context).pop(_currentProduct);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            onPressed: () {
+              Navigator.of(context).pop(_currentProduct);
+            },
           ),
-        ],
-      ),
-      body: BlocListener<ProductDetailBloc, ProductDetailState>(
-        listener: (context, state) {
-          if (state is ProductDetailSuccess) {
-            final message = state.type == ProductDetailSuccessType.updated
-                ? context.S.productUpdatedSuccessfullyMessage
-                : context.S.productDeletedSuccessfullyMessage;
-            showAppSnackBar(
-                context,
-                message +
-                    (state.isQueued ? context.S.offlineIndicator : ''));
-            if (state.type == ProductDetailSuccessType.updated) {
-              setState(() {
-                _currentProduct = state.updatedProduct!;
-              });
-              // Exit edit mode on successful update
-              if(_isEditing) {
-                _toggleEditMode();
-              }
-            } else if (state.type == ProductDetailSuccessType.deleted) {
-              Navigator.of(context).pop(true);
-            }
-          } else if (state is ProductUpdateFailure) {
-            showAppSnackBar(
-                context, context.S.failedToUpdateProductMessage, isError: true);
-          } else if (state is ProductDeleteFailure) {
-            showAppSnackBar(
-                context, context.S.failedToDeleteProductMessage, isError: true);
-          }
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(AppDimensions.medium),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ProductImage(
-                imageUrl: _currentProduct.imageUrl,
-                width: MediaQuery.of(context).size.width - (AppDimensions.medium * 2),
-                height: AppDimensions.productImageHeight,
+          title: Text(_isEditing
+              ? context.S.editProductDialogTitle
+              : _currentProduct.name),
+          actions: [
+            if (_isEditing)
+              IconButton(
+                icon: const Icon(Icons.cancel),
+                onPressed: _toggleEditMode,
+              )
+            else
+              IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: _toggleEditMode,
               ),
-              const SizedBox(height: AppDimensions.medium),
-              _isEditing ? _buildEditView() : _buildViewView(),
-            ],
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: _showDeleteConfirmationDialog,
+            ),
+          ],
+        ),
+        body: BlocListener<ProductDetailBloc, ProductDetailState>(
+          listener: (context, state) {
+            if (state is ProductDetailSuccess) {
+              final message = state.type == ProductDetailSuccessType.updated
+                  ? context.S.productUpdatedSuccessfullyMessage
+                  : context.S.productDeletedSuccessfullyMessage;
+              showAppSnackBar(context,
+                  message + (state.isQueued ? context.S.offlineIndicator : ''));
+              if (state.type == ProductDetailSuccessType.updated) {
+                setState(() {
+                  _currentProduct = state.updatedProduct!;
+                  // also update text controllers to reflect the new state
+                  _nameController.text = _currentProduct.name;
+                  _skuController.text = _currentProduct.sku;
+                  _locationController.text = _currentProduct.location ?? '';
+                  _imageFile = null; // Clear the local image file
+                });
+                // Exit edit mode on successful update
+                if (_isEditing) {
+                  _toggleEditMode();
+                }
+              } else if (state.type == ProductDetailSuccessType.deleted) {
+                Navigator.of(context).pop(true);
+              }
+            } else if (state is ProductUpdateFailure) {
+              showAppSnackBar(
+                  context, context.S.failedToUpdateProductMessage,
+                  isError: true);
+            } else if (state is ProductDeleteFailure) {
+              showAppSnackBar(
+                  context, context.S.failedToDeleteProductMessage,
+                  isError: true);
+            }
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(AppDimensions.medium),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (!_isEditing)
+                    ProductImage(
+                      imageUrl: _currentProduct.imageUrl,
+                      width: MediaQuery.of(context).size.width -
+                          (AppDimensions.medium * 2),
+                      height: AppDimensions.productImageHeight,
+                    ),
+                  const SizedBox(height: AppDimensions.medium),
+                  _isEditing ? _buildEditView() : _buildViewView(),
+                ],
+              ),
+            ),
           ),
         ),
+        floatingActionButton: _isEditing
+            ? FloatingActionButton.extended(
+                onPressed: _saveChanges,
+                label: Text(context.S.saveButtonText),
+                icon: const Icon(Icons.save),
+              )
+            : null,
       ),
-      floatingActionButton: _isEditing
-          ? FloatingActionButton.extended(
-              onPressed: _saveChanges,
-              label: Text(context.S.saveButtonText),
-              icon: const Icon(Icons.save),
-            )
-          : null,
     );
   }
 
@@ -200,6 +249,7 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
         ),
         const SizedBox(height: AppDimensions.small),
         Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Text(
               '${context.S.quantityLabel}: ',
@@ -220,7 +270,8 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
           ],
         ),
         const SizedBox(height: AppDimensions.small),
-        if (_currentProduct.location != null && _currentProduct.location!.isNotEmpty)
+        if (_currentProduct.location != null &&
+            _currentProduct.location!.isNotEmpty)
           Text(
             context.S.productLocationLabelWithColon(_currentProduct.location!),
             style: Theme.of(context).textTheme.titleMedium,
@@ -232,9 +283,37 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
   Widget _buildEditView() {
     return Column(
       children: [
+        GestureDetector(
+          onTap: _pickImage,
+          child: Container(
+            height: AppDimensions.productImageSmallSize,
+            width: AppDimensions.productImageSmallSize,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(AppDimensions.small),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: _imageFile != null
+                ? ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(AppDimensions.small),
+                    child: Image.file(
+                      _imageFile!,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                : ProductImage(
+                    imageUrl: _currentProduct.imageUrl,
+                    width: AppDimensions.productImageSmallSize,
+                    height: AppDimensions.productImageSmallSize,
+                  ),
+          ),
+        ),
+        const SizedBox(height: AppDimensions.medium),
         TextFormField(
           controller: _nameController,
-          decoration: InputDecoration(labelText: context.S.productNameLabel),
+          decoration:
+              InputDecoration(labelText: context.S.productNameLabel),
         ),
         const SizedBox(height: AppDimensions.medium),
         TextFormField(
@@ -244,7 +323,8 @@ class _ProductDetailViewState extends State<_ProductDetailView> {
         const SizedBox(height: AppDimensions.medium),
         TextFormField(
           controller: _locationController,
-          decoration: InputDecoration(labelText: context.S.productLocationLabel),
+          decoration:
+              InputDecoration(labelText: context.S.productLocationLabel),
         ),
       ],
     );
