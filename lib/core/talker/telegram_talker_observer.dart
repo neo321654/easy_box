e59b1b84699cc55b66bd0f476806285decd804ad
neo321654 <neo321654@rambler.ru>
2025-09-05 21:55:40 +0300

    fix: Finalize talker observer implementation

--- lib/core/talker/telegram_talker_observer.dart
+++ lib/core/talker/telegram_talker_observer.dart
@@ -5,16 +5,12 @@ class TelegramTalkerObserver extends TalkerObserver {
   final Dio _dio = Dio();
   final String _url = 'http://38.244.208.106:8000/log-client-error';
 
-  void _sendToTelegram(String message) {
-    try {
-      _dio.post(
-        _url,
-        data: {'message': message},
-      );
-    } catch (e) {
-      // Avoid loops, just print to console if sending fails
-      // ignore: avoid_print
-      print('Failed to send log to Telegram: $e');
+  @override
+  void onLog(TalkerData log) {
+    // The TalkerDioLogger logs 4xx and 5xx errors as a TalkerLog
+    // with a specific title. We check for that title here.
+    if (log is TalkerLog && log.title == 'http-error') {
+      _sendToTelegram(log.generateTextMessage());
     }
   }
 
@@ -26,5 +22,18 @@ class TelegramTalkerObserver extends TalkerObserver {
   @override
   void onException(TalkerException err) {
     _sendToTelegram(err.generateTextMessage());
+  }
+
+  void _sendToTelegram(String message) {
+    try {
+      _dio.post(
+        _url,
+        data: {'message': message},
+      );
+    } catch (e) {
+      // Avoid loops, just print to console if sending fails
+      // ignore: avoid_print
+      print('Failed to send log to Telegram: $e');
+    }
   }
 }
