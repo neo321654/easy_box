9439ceae857bf9042f6cca79a842a9757c32d5b2
neo321654 <neo321654@rambler.ru>
2025-09-08 09:25:43 +0300

    refactor: move image upload logic to viewset

--- easy_box_backend/api/serializers.py
+++ easy_box_backend/api/serializers.py
@@ -32,25 +32,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(write_only=True, required=False)
-
     class Meta:
         model = Product
         fields = '__all__'
-
-    def create(self, validated_data):
-        if 'image' in validated_data:
-            image = validated_data.pop('image')
-            upload_result = cloudinary.uploader.upload(image)
-            validated_data['image_url'] = upload_result['secure_url']
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        if 'image' in validated_data:
-            image = validated_data.pop('image')
-            upload_result = cloudinary.uploader.upload(image)
-            validated_data['image_url'] = upload_result['secure_url']
-        return super().update(instance, validated_data)
 
 class OrderLineSerializer(serializers.ModelSerializer):
     product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
--- easy_box_backend/api/views.py
+++ easy_box_backend/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from .models import User, Product, Order, OrderLine
 from .serializers import UserSerializer, ProductSerializer, OrderSerializer, OrderLineSerializer, CustomAuthTokenSerializer
+import cloudinary.uploader
 
 class CustomObtainAuthToken(ObtainAuthToken):
     serializer_class = CustomAuthTokenSerializer
@@ -30,12 +32,18 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['post'], parser_classes=[MultiPartParser, FormParser])
     def upload_image(self, request, pk=None):
         product = self.get_object()
-        serializer = self.get_serializer(product, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        image = request.data.get('image')
+        if not image:
+            return Response({'error': 'No image provided'}, status=400)
+
+        upload_result = cloudinary.uploader.upload(image)
+        product.image_url = upload_result['secure_url']
+        product.save()
+
+        serializer = self.get_serializer(product)
         return Response(serializer.data)
 
 class OrderViewSet(viewsets.ModelViewSet):
