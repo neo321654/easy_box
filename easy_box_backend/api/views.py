78a120c5ac56a8d1a5582acd221bfd4941049641
neo321654 <neo321654@rambler.ru>
2025-09-08 08:52:27 +0300

    feat: add /users/me endpoint

--- easy_box_backend/api/views.py
+++ easy_box_backend/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -19,6 +20,11 @@ class CustomObtainAuthToken(ObtainAuthToken):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        serializer = self.get_serializer(request.user, many=False)
+        return Response(serializer.data)
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
